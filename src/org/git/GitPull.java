744b5a2d6d3eb2d1246d9e6d5b3f3323bf9ecdf2
selvakumarst <m.selva.mdu@gmail.com>
2021-02-06 16:55:53 +0530

    new method added

---
+++ src/org/git/GitPull.java
@@ -0,0 +1,22 @@
+package org.git;
+
+public class GitPull {
+
+	public void firstprgm() {
+		
+		}
+	
+	public void secprgm() {
+		
+	}
+
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+	}
+
+}
